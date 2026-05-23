22476e3219cf9fe7f461baf24e0e4e44d0b086d3
kuratadeesu <96030065+kuratadeesu@users.noreply.github.com>
2026-05-24 01:41:55 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ const saveBtn = document.getElementById("saveBtn");
 const memoList = document.getElementById("memoList");
 const searchInput = document.getElementById("searchInput");
 const currentTag = document.getElementById("currentTag");
+const sortSelect = document.getElementById("sortSelect");
+const cancelEditButton = document.getElementById("cancelEditButton");
 
 let memos = JSON.parse(localStorage.getItem("memos")) || [];
-
 let selectedTag = "";
-
 let editIndex = null;
+let editingCardIndex = null;
 
 displayMemos();
+
+cancelEditButton.addEventListener("click", () => {
+  document.getElementById("editStatus").textContent = "";
+  editIndex = null;
+  cancelEditButton.classList.add("hidden");
+  clearForm();
+  document.getElementById("editStatus").classList.add("hidden");
+  editingCardIndex = null;
+  displayMemos();
+});
 
 saveBtn.addEventListener("click", () => {
   const situation = document.getElementById("situation").value;
@@ -23,8 +34,8 @@ saveBtn.addEventListener("click", () => {
   const year = now.getFullYear();
   const month = now.getMonth() + 1;
   const day = now.getDate();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
   const date = `${year}/${month}/${day} ${hours}:${minutes}`;
 
   const memo = {
@@ -32,7 +43,8 @@ saveBtn.addEventListener("click", () => {
     feeling,
     reason,
     nextAction,
-    date,
+    createdAt: date,
+    updatedAt: date,
     tag,
     emotion
   };
@@ -40,18 +52,29 @@ saveBtn.addEventListener("click", () => {
   if (editIndex === null) {
     memos.unshift(memo);
   } else {
+    memo.createdAt = memos[editIndex].createdAt;
+    memo.updatedAt = date;
     memos.splice(editIndex, 1);
     memos.unshift(memo);
+
     editIndex = null;
   }
-  
 
   localStorage.setItem("memos", JSON.stringify(memos));
 
+  document.getElementById("editStatus").textContent = "";
+  cancelEditButton.classList.add("hidden");
+  editStatus.classList.add("hidden");
+  editingCardIndex = null;
+  
   displayMemos();
 
   clearForm();
 });
+
+sortSelect.addEventListener("change", () => {
+  displayMemos();
+}); 
 
 searchInput.addEventListener("input", () => {
   displayMemos();
@@ -72,23 +95,49 @@ function displayMemos() {
 
   const filteredMemos = memos.filter((memo) => {
 
-    const matchKeyword  =
+    const matchKeyword =
       memo.situation.includes(keyword) ||
       memo.feeling.includes(keyword) ||
       memo.reason.includes(keyword) ||
       memo.nextAction.includes(keyword) ||
-      memo.date.includes(keyword);
+      (memo.createdAt || "").includes(keyword) ||
+      (memo.updatedAt || "").includes(keyword);
 
-    const matchTag = 
-    selectedTag === "" ||
-    memo.tag === selectedTag;
+    const matchTag =
+      selectedTag === "" ||
+      memo.tag === selectedTag;
 
     return matchKeyword && matchTag;
+  });
+
+  const sortValue = sortSelect.value;
+
+  filteredMemos.sort((a,b) => {
+  if (sortValue === "created-desc") {
+
+    return b.createdAt.localeCompare(a.createdAt);
+
+  } else if (sortValue === "created-asc") {
+
+    return a.createdAt.localeCompare(b.createdAt);
+
+  } else if (sortValue === "updated-desc") {
+
+    return b.updatedAt.localeCompare(a.updatedAt);
+
+  } else if (sortValue === "updated-asc") {
+
+    return a.updatedAt.localeCompare(b.updatedAt);
+  }
   });
 
   filteredMemos.forEach((memo, index) => {
     const card = document.createElement("div");
     card.classList.add("memo-card");
+
+    if (editingCardIndex === index) {
+      card.classList.add("editing-card");
+    }
 
     let emotionColor = "";
 
@@ -105,32 +154,50 @@ function displayMemos() {
     }
 
     card.innerHTML = `
-      <h3>${memo.date}</h3>
-      <p><strong>感情</strong><br>
-        <span style="color:${emotionColor}">
-          ${memo.emotion}
-        </span>
-      </p>
-      <p><strong>何があった？</strong><br>${memo.situation}</p>
-      <p><strong>相手はどう感じた？</strong><br>${memo.feeling}</p>
-      <p><strong>なぜその行動をした？</strong><br>${memo.reason}</p>
-      <p><strong>次どうする？</strong><br>${memo.nextAction}</p>
-      <p>
-        <strong>タグ</strong><br>
-        <button class="tag-button" onclick="searchTag('${memo.tag}')">
-          ${memo.tag}
+    ${editingCardIndex === index? `<div class="editing-badge">
+       ✏️ 編集中
+      </div>`
+      : ""
+    }
+
+      <div class="memo-header">
+        <h3>登録日時 ${memo.createdAt || memo.date}</h3>
+        <h3>更新日時 ${memo.updatedAt || "未更新"}</h3>
+        <p><strong>感情</strong><br>
+          <span style="color:${emotionColor}">
+            ${memo.emotion}
+          </span>
+        </p>
+        <p><strong>何があった？</strong><br>${memo.situation}</p>
+        <p id="toggle-button-${index}" class="toggle-style" onclick="toggleMemo(${index})">
+          ▼続きを表示
+        </p>
+      </div>
+      <div id="memo-detail-${index}" class="hidden">
+        <p><strong>相手はどう感じた？</strong><br>${memo.feeling}</p>
+        <p><strong>なぜその行動をした？</strong><br>${memo.reason}</p>
+        <p><strong>次どうする？</strong><br>${memo.nextAction}</p>
+        <p>
+          <strong>タグ</strong><br>
+          <button class="tag-button" onclick="searchTag('${memo.tag}')">
+            ${memo.tag}
+          </button>
+        </p>
+        <button onclick="deleteMemo(${index})">
+          削除
         </button>
-      </p>
-      <button onclick="deleteMemo(${index})">
-        削除
-      </button>
-      <button onclick="editMemo(${index})">
-        編集
-      </button>
+        <button onclick="editMemo(${index})">
+          編集
+        </button>
+        <p  class="toggle-style" onclick="toggleMemo(${index})">
+          ▲閉じる
+        </p>
+      </div>
     `;
 
     memoList.appendChild(card);
   });
+
 }
 
 function clearForm() {
@@ -144,9 +211,9 @@ function clearForm() {
 
 function deleteMemo(index) {
 
-  const isDelete = confirm("本当に削除しますか？"); 
+  const isDelete = confirm("本当に削除しますか？");
 
-  if(isDelete) {
+  if (isDelete) {
     memos.splice(index, 1);
 
     localStorage.setItem("memos", JSON.stringify(memos));
@@ -168,10 +235,33 @@ function clearTag() {
 function editMemo(index) {
   const memo = memos[index];
 
+  cancelEditButton.classList.remove("hidden");
+
   document.getElementById("situation").value = memo.situation;
   document.getElementById("feeling").value = memo.feeling;
   document.getElementById("reason").value = memo.reason;
   document.getElementById("nextAction").value = memo.nextAction;
 
   editIndex = index;
+  editingCardIndex = index;
+
+  document.getElementById("formArea").scrollIntoView({
+    behavior: "smooth"
+  });
+
+  const editStatus = document.getElementById("editStatus");
+  editStatus.textContent = "現在編集中です";
+  editStatus.classList.remove("hidden");
+
+  displayMemos();
+
+}
+
+function toggleMemo(index) {
+  console.log(`Toggle memo at index: ${index}`);
+  const detail = document.getElementById(`memo-detail-${index}`);
+  const toggleButton = document.getElementById(`toggle-button-${index}`);
+
+  detail.classList.toggle("hidden");
+  toggleButton.classList.toggle("hidden");
 }
